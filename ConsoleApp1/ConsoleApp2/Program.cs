db04697458ec67e30d7f02cf680793c44fb890d8
Carl in 't Veld <carl@intveld.nl>
2023-04-25 14:13:45 +0200

    HierarchyId repro

---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp2.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var entities = new Entities();
+            var result = entities.AansluitingsObject.ToList();
+
+        }
+    }
+}
